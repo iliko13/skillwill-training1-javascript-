3865c9b56848a2823f01678763b5cce4d65ec30f
iliko13 <107374131+iliko13@users.noreply.github.com>
2022-11-25 19:49:26 +0400

    13

--- src/script.js
+++ src/script.js
@@ -44,65 +44,151 @@
 // console.log(null === undefined); //false
 
 
-//ფუნქციები
-// 1) ფუნქცია აბრუნებს მნიშვნელობას
-// 2) ფუნქცია არ აბრუნებს მნიშვნელობას
-// 3) ფუნქცია ხან აბრუნებს მნიშვნელობას ხან არა
-//მაგალითები:
-function calculateSum1(a , b) {
-    return a + b;
-}
+// //ფუნქციები
+// // 1) ფუნქცია აბრუნებს მნიშვნელობას
+// // 2) ფუნქცია არ აბრუნებს მნიშვნელობას
+// // 3) ფუნქცია ხან აბრუნებს მნიშვნელობას ხან არა
+// //მაგალითები:
+// function calculateSum1(a , b) {
+//     return a + b;
+// }
 
-function calculateSum2(a , b) {
-    console.log(a + b);
-}
+// function calculateSum2(a , b) {
+//     console.log(a + b);
+// }
 
-function calculateSum3(a ,b) {
-    if (a !== b){
-        return a + b
+// function calculateSum3(a ,b) {
+//     if (a !== b){
+//         return a + b
+//     }
+// }
+
+// console.log(calculateSum1(4, 5)); //9
+// calculateSum2(4, 5) //9
+// console.log(calculateSum3(4, 5))//9
+// console.log(calculateSum3(5, 5)) // undefined
+
+
+// //ფუნქცია 1
+// function calculateSum1(a, b){
+//     return(a + b);
+// } //ეს ფუნქცია არის ფუნქცია, რომელსაც დავარქვით სახელად aclculateSum1
+
+// //ფუნქცია 2
+// const calculateSum2 = function(a, b) {
+//     return a + b;
+// }// ეს არის ანონიმური ფუნქცია, რომელიც შენახული გვაქვს ცვლადში სახელად calculateSum2
+
+// console.log(calculateSum1(4, 5)); //9
+// console.log(calculateSum2(4, 5)); //9
+
+// //განსხვავებები:
+// //calculateSum1 გამოძახება შეგვიძია ინიციალიზაციამდე
+// //calculateSum2 გამოძახება შეგვიძლია მხოლოდ მისი ინიციალიზაციის შემდეგ
+
+// console.log(calculateSum1(4, 5)); //9
+// console.log(calculateSum2(4, 5)); // გამოიწვევს შეცდომას
+
+// function calculateSum1(a, b) {
+//     return a + b;
+// }
+
+// function calculateSum2(a, b) {
+//     return a + b;
+// }
+
+// //მაგალითები:
+// let company = "skillwill";
+
+// function changeCompany() {
+//     company = "sweeft";
+// }
+
+// changeCompany();
+// console.log(company); // sweeft
+
+
+
+//დავალება 1
+// დაწერეთ ფუნქცია, რომელიც მიიღებს a და b პარამეტრებს და დააბრუნებს ტექსტს
+// "ტოლია" თუ a უდრის b-ს, ხოლო წინააღდეგ შემთხვევაში დააბრუნებს "არ არის ტოლი".
+// გაითვალისწინეთ, რომ a და ბ ყოველთვის ერთი და იგივე ტიპის არ არის.
+
+
+function Homework1(a, b) {
+    if (a === b) {
+        return "ტოლია"
+    } else {
+        return "არ არის ტოლი"
     }
 }
 
-console.log(calculateSum1(4, 5)); //9
-calculateSum2(4, 5) //9
-console.log(calculateSum3(4, 5))//9
-console.log(calculateSum3(5, 5)) // undefined
+console.log(Homework1(4, 4)) // ტოლია
+console.log(Homework1(3, 4)) // არ არის ტოლი
 
+//დავალება 2
+// დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს ტემპერატურას ფარენჰეიტებში
+// და დააბრუნებს ტემპერატურას ცელსიუსში. თუ პარამეტრი არ არის რიცხვითი 
+// მონაცემი დააბრუნეთ false.
 
-//ფუნქცია 1
-function calculateSum1(a, b){
-    return(a + b);
-} //ეს ფუნქცია არის ფუნქცია, რომელსაც დავარქვით სახელად aclculateSum1
+function toCelsius(fahrenheit) {
+    let fTemp = fahrenheit;
+    let ftoCel = 5 / 9 * (fTemp - 32);
+    let message =fTemp + "is" + ftoCel;
+    
+    
+    if (typeof(fahrenheit) === "string") {
+        return "False"
+      } else {
+        return message
+      }
+  }
 
-//ფუნქცია 2
-const calculateSum2 = function(a, b) {
-    return a + b;
-}// ეს არის ანონიმური ფუნქცია, რომელიც შენახული გვაქვს ცვლადში სახელად calculateSum2
+console.log(toCelsius("ilia")) // false
+console.log(toCelsius(10)) // -12.2
 
-console.log(calculateSum1(4, 5)); //9
-console.log(calculateSum2(4, 5)); //9
-
-//განსხვავებები:
-//calculateSum1 გამოძახება შეგვიძია ინიციალიზაციამდე
-//calculateSum2 გამოძახება შეგვიძლია მხოლოდ მისი ინიციალიზაციის შემდეგ
-
-console.log(calculateSum1(4, 5)); //9
-console.log(calculateSum2(4, 5)); // გამოიწვევს შეცდომას
-
-function calculateSum1(a, b) {
-    return a + b;
+//მეორე ვარიანტი
+function temperature (fahr) {
+    if (typeof fahr == "number") {
+        return (fahr - 32) / 1.8
+    } else {
+        return "false"
+    }
 }
 
-function calculateSum2(a, b) {
-    return a + b;
+
+//დავალება 3
+// დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიღებს a (პირველი რიცხვი), 
+// b(მეორე რიცხვი) და operation(+,-,*,/) და დააბრუნებს ახალ მნიშვნელობას,
+// რომელიც მიიღება ამ ორ რიცხვზე operation ცვლადში განსაზღვრული ოპერაციით.
+// თუ a და b არიან რიცხვები ან თუ operation ცვლადში არის უცნობი ოპერაია
+// დააბრუნეთ false.
+function homework3 (a, b, operation) {
+    if (operation === "*") return a * b;
+    if (operation === "-") return a - b;
+    if (operation === "+") return a + b;
+    if (operation === "/") return b === 0 ? "can not divide" : a / b;
+    if (operation !== "/" && operation !== "*" && operation !== "-" && operation !== "+")
+    return "Please enter correct number"
 }
 
-//მაგალითები:
-let company = "skillwill";
+console.log(homework3(2, 5, "*")) // 10
+console.log(homework3(10, 5, "/")) // 2
+console.log(homework3(2, 5, "+")) // 7
+console.log(homework3(2, 5, "-")) // -3
+console.log(homework3(2, 5, "&")) //Please enter correct number
 
-function changeCompany() {
-    company = "sweeft";
+//მეორე ვარიანტი
+function myFunction (a, b, operation) {
+    if (operation == "+") {
+        return a + b
+    } else if (operation == "-") {
+        return a - b
+    } else if (operation == "*") {
+        return a * b
+    } else if (operation == "/") {
+        return a / b
+    } else {
+        return "false"
+    }
 }
-
-changeCompany();
-console.log(company); // sweeft
